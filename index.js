244f8224719324f446622bbc055544c05320f3c4
victorstarkmar42 <v_chan@outlook.es>
2020-08-08 17:33:53 -0500

    Modificando index, agregando .env

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const {
+    PORT
+}
+process.env
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http: //localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening at http: //localhost:${PORT}`)
 })
